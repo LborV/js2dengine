6a29a973ff6409d2d58cb5b4a46523667a481495
boriss <boriss@mendo.lv>
2018-12-03 11:48:38 +0200

    comments

--- engine.js
+++ engine.js
@@ -1,6 +1,21 @@
+/**
+ * 
+ * @param string style -> custom css styles for canvas 
+ * @param bool startAuto -> start engine(drawing) automatically 
+ * @param int width -> canvas width 
+ * @param int height -> canvas height
+ */
 function engine(style = undefined, startAuto = undefined, width = undefined, height = undefined) {
     //Initialization
+    
+    /**
+     * link to the engine object
+     */
     let _engine = this;
+
+    /**
+     * loaded web page
+     */
     _engine.doc = document;
 
     if(startAuto != undefined) {
@@ -17,6 +32,9 @@ function engine(style = undefined, startAuto = undefined, width = undefined, hei
         _engine.height = _engine.doc.body.clientHeight;
     }
 
+    /**
+     * Creating canvas object 
+     */
     _engine.canv = _engine.doc.createElement('canvas');
     _engine.canv.width = _engine.width;
     _engine.canv.height = _engine.height;
@@ -28,13 +46,28 @@ function engine(style = undefined, startAuto = undefined, width = undefined, hei
         _engine.canv.style.cssText = style;
     }
 
+    /**
+     * Insert canvas into HTML
+     */
     _engine.doc.body.appendChild(_engine.canv);
+
+    /**
+     * Context of canvas
+     */
     _engine.ctx = _engine.canv.getContext('2d');
 
     //Logger
+    /**
+     * Class to help output in console styled messages
+     * and enable/disable console logs
+     */
     _engine.logger = function(){
         this.oldConsoleLog = null;
 
+        /**
+         * If console logs enabled @return void, else
+         * enable console logs
+         */
         this.enable = function(){
             if(this.oldConsoleLog == null)
                 return;
@@ -42,50 +75,102 @@ function engine(style = undefined, startAuto = undefined, width = undefined, hei
             window['console']['log'] = this.oldConsoleLog;
         }
 
+        /**
+         * Show green message
+         * @param string message -> message that will be displayed 
+         */
         this.okMessage = function(msg){
             console.log('%c ' + msg, 'color: green;');
         }
 
+        /**
+         * Show red message
+         * @param string message -> message that will be displayed 
+         */
         this.errorMessage = function(msg){
             console.log('%c ' + msg, 'color: red;');
         }
 
+        /**
+         * Show blue message
+         * @param string message -> message that will be displayed 
+         */
         this.infoMessage = function(msg){
             console.log('%c ' + msg, 'color: blue;');
         }
 
+        /**
+         * Show custom styled message
+         * @param string message -> message that will be displayed
+         * @param string style -> custom css style
+         */
         this.customStyledMessage = function(msg, style){
             console.log('%c ' + msg, style);
         }
 
+        /**
+         * Disable console logs
+         */
         this.disable = function(){
             this.oldConsoleLog = _engine.engineLogger.errorMessage;
             window['console']['log'] = function() {};
         }
 
+        /**
+         * New object created
+         */
         this.okMessage('Logger created!');
     };
 
+    /**
+     * New object to display messages inside a engine to debuging
+     */
     _engine.engineLogger = new _engine.logger();
+    _engine.engineLogger.enable();
 
     //Loading
     _engine.isLoaded = false;
+    
+    /**
+     * Function that works while page loading
+     * Should be redefined
+     */
     _engine.loading = function(){
         _engine.engineLogger.errorMessage('Loading function is not defined');
     }
+
+    /**
+     * Function that work once when page loaded
+     * Should be redefined
+     */
     _engine.onload = function(){
         _engine.engineLogger.errorMessage('On load function is not defined');
     }
+
+    /**
+     * Function that control loading
+     */
     window.onload = function(){
         _engine.isLoaded = true;
         _engine.onload();
     }
 
     //Drawing
+    /**
+     * Game loop
+     * Should be redefined
+     */
     _engine.update = function(){
         _engine.engineLogger.errorMessage('Update function is not declared!');
     }
 
+    /**
+     * Display update function if loaded, else
+     * display loading function
+     * 
+     * Clear screen
+     * Recursive
+     */
     _engine.draw = function(){
         _engine.ctx.clearRect(0, 0, _engine.width, _engine.height);
         if(_engine.isLoaded) {
@@ -96,6 +181,9 @@ function engine(style = undefined, startAuto = undefined, width = undefined, hei
         requestAnimationFrame(_engine.draw);
     }
 
+    /**
+     * Help function that work once when page loaded
+     */
     _engine.start = function(){
         _engine.keyBoard();
         _engine.mouse();
@@ -103,6 +191,15 @@ function engine(style = undefined, startAuto = undefined, width = undefined, hei
     }
 
     //Rectangle
+    /**
+     * Class of filled rectangle
+     * @param array params:
+     *  x -> left top corner position X
+     *  y -> left top corner position Y
+     *  width -> rectangle width
+     *  height -> rectangle height
+     *  color -> CSS fill color
+     */
     _engine.fillBox = function(params){
         let _fillBox = this;
 
@@ -113,18 +210,34 @@ function engine(style = undefined, startAuto = undefined, width = undefined, hei
         _fillBox.color = params.color;
 
         _fillBox.hovered = false;
+        /**
+         * Function work when cursor on/inside rectangle line
+         * Should be redefined
+         */
         _fillBox.hover = function(){
             _engine.engineLogger.errorMessage('Hover function is not declared');
         }
 
+        /**
+         * Function work when cursor escape rectangle line
+         * Should be redefined
+         */
         _fillBox.unhover = function(){
             _engine.engineLogger.errorMessage('Unhover function is not declared');
         }
 
+        /**
+         * Function work when cursor on/inside rectangle and mouse pressed
+         * Should be redefined
+         */
         _fillBox.onDown = function(){
             _engine.engineLogger.errorMessage('On mouse down function is not declared');
         }
 
+        /**
+         * Function work when cursor on/inside rectangle mouse was pressed and than released
+         * Should be redefined
+         */
         _fillBox.onUp = function(){
             _engine.engineLogger.errorMessage('On mouse up function is not declared');
         }
@@ -157,6 +270,16 @@ function engine(style = undefined, startAuto = undefined, width = undefined, hei
         }
     }
 
+    /**
+     * Class of stroke rectangle
+     * @param array params:
+     *  x -> left top corner position X
+     *  y -> left top corner position Y
+     *  width -> rectangle width
+     *  height -> rectangle height
+     *  color -> CSS line color
+     *  lineWidth -> line width
+     */
     _engine.strokeBox = function(params){
         let _strokeBox = this;
 
@@ -168,18 +291,34 @@ function engine(style = undefined, startAuto = undefined, width = undefined, hei
         _strokeBox.lineWidth = params.lineWidth;
 
         _strokeBox.hovered = false;
+        /**
+         * Function work when cursor on/inside rectangle line
+         * Should be redefined
+         */
         _strokeBox.hover = function(){
             _engine.engineLogger.errorMessage('Hover function is not declared');
         }
 
+        /**
+         * Function work when cursor escape rectangle line
+         * Should be redefined
+         */
         _strokeBox.unhover = function(){
             _engine.engineLogger.errorMessage('Unhover function is not declared');
         }
 
+        /**
+         * Function work when cursor on/inside rectangle and mouse pressed
+         * Should be redefined
+         */
         _strokeBox.onDown = function(){
             _engine.engineLogger.errorMessage('On mouse down function is not declared');
         }
 
+        /**
+         * Function work when cursor on/inside rectangle mouse was pressed and than released
+         * Should be redefined
+         */
         _strokeBox.onUp = function(){
             _engine.engineLogger.errorMessage('On mouse up function is not declared');
         }
@@ -218,6 +357,15 @@ function engine(style = undefined, startAuto = undefined, width = undefined, hei
     }
 
     //Sprite
+    /**
+     * Class of sprite
+     * @param array params:
+     *  x -> left top corner position X
+     *  y -> left top corner position Y
+     *  width -> rectangle width
+     *  height -> rectangle height
+     *  src -> source of image
+     */
     _engine.sprite = function(params){
         let _sprite = this;
 
@@ -229,22 +377,42 @@ function engine(style = undefined, startAuto = undefined, width = undefined, hei
         _sprite.height = params.height;
 
         _sprite.hovered = false;
+        /**
+         * Function work when cursor on/inside rectangle line
+         * Should be redefined
+         */
         _sprite.hover = function(){
             _engine.engineLogger.errorMessage('Hover function is not declared');
         }
 
+        /**
+         * Function work when cursor escape rectangle line
+         * Should be redefined
+         */
         _sprite.unhover = function(){
             _engine.engineLogger.errorMessage('Unhover function is not declared');
         }
 
+        /**
+         * Function work when cursor on/inside rectangle and mouse pressed
+         * Should be redefined
+         */
         _sprite.onDown = function(){
             _engine.engineLogger.errorMessage('On mouse down function is not declared');
         }
 
+        /**
+         * Function work when cursor on/inside rectangle mouse was pressed and than released
+         * Should be redefined
+         */
         _sprite.onUp = function(){
             _engine.engineLogger.errorMessage('On mouse up function is not declared');
         }
 
+        /**
+         * If width and height not defined -> getting image sizes
+         * Without width and height didnt work hover unhover onDown onUp functions
+         */
         _sprite.image.onload = function(){
             if(_sprite.width == undefined || _sprite.height == undefined) {
                 _sprite.width = this.width;
@@ -280,7 +448,14 @@ function engine(style = undefined, startAuto = undefined, width = undefined, hei
 
     //Keyboard
     _engine.isKeyboardInit = false;
+    /**
+     * Global array that contain pressed keys codes
+    */
     _engine.pressedKeys = {};
+
+    /**
+     * Class that add event listeners for keyboard
+     */
     _engine.keyBoard = function(){
         if(_engine.isKeyboardInit) {
             return;
@@ -298,11 +473,21 @@ function engine(style = undefined, startAuto = undefined, width = undefined, hei
 
     //Mouse
     _engine.isMouseInit
+    /**
+     * Global array that contain:
+     *  x -> position of cursor X
+     *  y -> position of cursor Y
+     *  down -> true if mouse clicked, false if not
+     */
     _engine.mouseCoordinates = {
         x: null,
         y: null,
         down: false
     };
+
+    /**
+     * Class that add event listeners for mouse
+     */
     _engine.mouse = function(){
         if(_engine.isMouseInit) {
             return;
@@ -325,6 +510,15 @@ function engine(style = undefined, startAuto = undefined, width = undefined, hei
     }
 
     //Text
+    /**
+     * @param array params:
+     *  x -> X position
+     *  y -> Y position
+     *  font -> CSS font
+     *  color -> CSS color
+     *  align -> CSS align
+     *  baseLine -> CSS text baseline
+     */
     _engine.fillTextLine = function(params){
         let _fillTextLine = this;
 
@@ -348,6 +542,14 @@ function engine(style = undefined, startAuto = undefined, width = undefined, hei
     }
 
     //Math things
+    /**
+     * @param int x1 -> x coordinate of first point
+     * @param int y1 -> y coordinate of first point
+     * @param int x2 -> x coordinate of second point
+     * @param int y2 -> y coordinate of second point
+     * 
+     * @return distance between points
+     */
     _engine.distanceBetweenTwoPoints = function(x1, y1, x2, y2){
         return Math.sqrt( (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
     }
