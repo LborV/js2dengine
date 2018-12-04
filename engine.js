2adce3d1a3d4a721b1cfe7eb4797c5b591f9b6b5
boriss <boriss@mendo.lv>
2018-12-04 13:04:53 +0200

    update: example

--- engine.js
+++ engine.js
@@ -831,7 +831,7 @@ function engine(style = undefined, startAuto = undefined, width = undefined, hei
         }
 
         //Left bottom corner
-        if(rect1.x >= rect2.x && rect1.x <= rect2.x + rect2.width && rect1.y + rect1.height >= rect2.y && rect1.y + rect1.height<= rect2.y + rect2.height) {
+        if(rect1.x >= rect2.x && rect1.x <= rect2.x + rect2.width && rect1.y + rect1.height >= rect2.y && rect1.y + rect1.height <= rect2.y + rect2.height) {
             return true;
         }
 
